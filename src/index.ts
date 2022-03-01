8bfc352fd872269cf8e90ee10c61f878172a6c30
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-03-01 15:12:00 +0100

    Add an home page and an artist details page

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,49 @@
 import "dotenv/config";
+import express from "express";
+import nunjucks from "nunjucks";
 import { MongoClient } from "mongodb";
-import { createArtist, createSong } from "./createSong";
+// import { createArtist, createSong } from "./createSong";
 
 const databaseUrl = process.env.MONGODB_DATABASE_URL || "";
 const client = new MongoClient(databaseUrl);
 
-client.connect().then((client) => {
-  const db = client.db();
+const app = express();
 
-  // createArtist(db, "Calliope", "Mori");
-  createSong(db, "Nameless Sadness", 2021, "IRyS").then(() => {
-    client.close();
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+});
+
+app.use(express.static("public"));
+
+app.set("view engine", "njk");
+
+// Home page
+app.get("/", (req, response) => {
+  client.connect().then(async (client) => {
+    const db = client.db();
+    const artists = await db.collection("artists").find().toArray();
+
+    response.render("home", { artists: artists });
   });
+});
+
+app.get("/artist/:slug", (req, response) => {
+  const result = req.params.slug;
+
+  client.connect().then(async (client) => {
+    const db = client.db();
+    const artist = await db.collection("artists").findOne({ lastName: result });
+    if (artist) {
+      const songs = await db
+        .collection("songs")
+        .find({ _id: { $in: artist.songs } })
+        .toArray();
+      response.render("artistDetail", { artist: artist, songs: songs });
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
 });
